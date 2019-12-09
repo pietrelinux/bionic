171acab24d2458c2d7f2d59147d7c1a242e8c244
pietre.linux <pietre.linux@gmail.com>
2019-12-09 02:17:39 +0100

    Update bionic.sh

--- bionic.sh
+++ bionic.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
-
-mkdir /bionic
+sudo apt-get install debootstrap libc6-dev-arm64-cross gcc-aarch64-linux-gnu 
+qemu-aarch64mkdir /bionic
 dd if=/dev/zero of=bionic.img bs=1 count=0 seek=4000M
-mkfs.ext4 -b 4096 -F bionic.img
+mkfs.btrfs -F bionic.img
 chmod 777 bionic.img
 mount -o loop bionic.img /bionic
-apt remove -y debootstrap
-
-wget http://cz.archive.ubuntu.com/ubuntu/pool/main/d/debootstrap/debootstrap_1.0.95_all.deb
-wget http://cz.archive.ubuntu.com/ubuntu/pool/main/u/ubuntu-keyring/ubuntu-keyring_2018.02.28_all.deb
-dpkg -i *.deb
-
 debootstrap --arch=arm64 --foreign bionic /bionic
-
 sudo mount -o bind /dev /bionic/dev && sudo mount -o bind /dev/pts /bionic/dev/pts && sudo mount -t sysfs sys /bionic/sys && sudo mount -t proc proc /bionic/proc
-> config.sh
-cat <<+ >> config.sh
+cp /usr/bin/qemu-arm-static /bionic/usr/bin
+cp /usr/bin/qemu-system-common /bionic/usr/bin
+cp /etc/resolv.conf /bionic/etc
+> /home/config.sh
+cat <<+ >> /home/config.sh
 #!/bin/sh
-
+echo " Configurando debootstrap segunda fase"
+sleep 3
 /debootstrap/debootstrap --second-stage
 export LANG=C
 echo "deb http://ports.ubuntu.com/ bionic main restricted universe multiverse" > /etc/apt/sources.list
@@ -27,22 +24,56 @@ echo "deb http://ports.ubuntu.com/ bionic-backports main restricted universe mul
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 echo "Europe/Berlin" > /etc/timezone
 echo "bionic" >> /etc/hostname
+echo "127.0.0.1 bionic localhost
+::1 ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts" >> /etc/hosts
+echo "auto lo
+iface lo inet loopback" >> /etc/network/interfaces
+echo "/dev/mmcblk0p1 /	   ext4	    errors=remount-ro,noatime,nodiratime 0 1" >> /etc/fstab
+echo "tmpfs    /tmp        tmpfs    nodev,nosuid,mode=1777 0 0" >> /etc/fstab
+echo "tmpfs    /var/tmp    tmpfs    defaults    0 0" >> /etc/fstab	
+cat <<END > /etc/apt/apt.conf.d/71-no-recommends
+APT::Install-Recommends "0";
+APT::Install-Suggests "0";
+END
+apt-get update
+echo "Reconfigurando parametros locales"
+sleep 1
 locale-gen es_ES.UTF-8
 export LC_ALL="es_ES.UTF-8"
 update-locale LC_ALL=es_ES.UTF-8 LANG=es_ES.UTF-8 LC_MESSAGES=POSIX
 dpkg-reconfigure locales
-locale-gen es_ES.UTF-8
-adduser bionic
-addgroup bionic sudo 
-addgroup bionic adm
-addgrouo bioni users
-addgroup bionic video
-+
-
-chmod +x  config.sh
-cp config.sh /bionic/home
-chroot /bionic /bin/sh -i ./home/config.sh && exit 
-
-
-apt update
+dpkg-reconfigure -f noninteractive tzdata
 apt-get upgrade -y
+sudo apt-get install wireless-tools iw ubuntu-desktop -y
+rm -f /var/lib/dpkg/info/udev.post*
+rm -f /var/lib/dpkg/info/udev.pre*
+apt-get -f install
+apt-get clean
+adduser bionic
+addgroup bionic sudo
+addgroup bionic adm
+addgroup bionic users
++
+chmod +x  /home/config.sh
+sudo cp  /home/config.sh /bionic/home
+chroot /bionic /usr/bin/qemu-system-arm /bin/sh -i ./home/config.sh
+rm /home/config.sh
+sudo umount /bionic/dev/pts
+sleep 3
+sync
+sudo umount /bionic/dev
+sleep 3
+sync
+sudo umount /bionic/proc
+sleep 3
+sync
+sudo umount /bionic/sys
+sleep 3
+sync
+umount /bionic
+exit
